d8cba5a8d45271a5507d859a3efb685830120fb5
Daniel <dluchaup@gmail.com>
2015-03-06 14:23:22 -0500

    v3

--- callgraph.ml
+++ callgraph.ml
@@ -2,11 +2,30 @@ open Core_kernel.Std
 open Bap.Std
 open Program_visitor
 
-let main project =
-  print_endline "hello, BAP";
-  Table.iter project.symbols ~f:print_endline;
-  project
+module Callgraph = struct
+  type t=project
+  module V = struct
+    type t = string
+  end
+  module E = struct
+    type t = string*string
+    let src = fst
+    let dst = snd
+  end
+  (* ... all other functions. Example: *)
+  let graph_attributes _ = []
+                           
+  let iter_vertex _ _ = ()
+  let iter_edges_e  _ _ = ()
+                          
+  (** Graph, vertex and edge attributes. *)
+    
+  let default_vertex_attributes _ = []
+  let vertex_name vt = vt
+  let vertex_attributes _ = []
+      
+  let get_subgraph  _ = None
+  let default_edge_attributes _ = []
+  let edge_attributes _ = []
 
-let () = register main
-
-
+end
