c15c0b1d74c8dbead2c425ec4d03c75da42b7535
Daniel <dluchaup@gmail.com>
2015-03-06 14:06:13 -0500

    v2

--- callgraph.ml
+++ callgraph.ml
@@ -2,7 +2,9 @@ open Core_kernel.Std
 open Bap.Std
 open Program_visitor
 
-let main project = print_endline "hello, BAP";
+let main project =
+  print_endline "hello, BAP";
+  Table.iter project.symbols ~f:print_endline;
   project
 
 let () = register main
